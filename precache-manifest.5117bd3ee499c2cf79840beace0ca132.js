80884c1c5c8ab5f47ea02ed8382089738985eb25
Duke Capitan <dukecapitan@gmail.com>
2019-12-05 00:50:36 +0100

    Updates

---
+++ precache-manifest.5117bd3ee499c2cf79840beace0ca132.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac02d5cb6c1354068b60a027866cbb1",
+    "url": "/reduxapp/index.html"
+  },
+  {
+    "revision": "424907885cd730fc15d6",
+    "url": "/reduxapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "49fb1ef5af62c6cddad6",
+    "url": "/reduxapp/static/css/main.027c7fad.chunk.css"
+  },
+  {
+    "revision": "424907885cd730fc15d6",
+    "url": "/reduxapp/static/js/2.b2c46fc9.chunk.js"
+  },
+  {
+    "revision": "49fb1ef5af62c6cddad6",
+    "url": "/reduxapp/static/js/main.c9fc497f.chunk.js"
+  },
+  {
+    "revision": "02c06657e0d0b8638999",
+    "url": "/reduxapp/static/js/runtime-main.d68ed33f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reduxapp/static/media/logo.25bf045c.svg"
+  }
+]);
